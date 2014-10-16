7cd8b7ac3989fe78351a3ad15b1ed1a6002ceb59
Vedant Tikku <allrounderonit007@gmail.com>
2014-10-17 03:57:59 +0530

    Update pyGet.py

--- pyGet.py
+++ pyGet.py
@@ -25,29 +25,22 @@ while 1:
 	f.write (speed[0])
 	f.close()
 	
-	if percentChange < -5:
+	if percentChange < -1:
 		print 'green'
 		GPIO.output("P8_10", GPIO.HIGH)
 		GPIO.output("P8_14", GPIO.LOW)
 		GPIO.output("P8_18",GPIO.LOW)
 		time.sleep(10)
 	else: 
-		if percentChange in range(0,-6,-1):
+		if percentChange > 0:
+			print 'Orange'
+			GPIO.output("P8_10", GPIO.LOW)
+			GPIO.output("P8_14", GPIO.LOW)
+			GPIO.output("P8_18",GPIO.HIGH)
+			time.sleep(10)
+		else:
 			print 'red'
 			GPIO.output("P8_10", GPIO.LOW)
 			GPIO.output("P8_14", GPIO.HIGH)
 			GPIO.output("P8_18",GPIO.LOW)
 			time.sleep(10)
-		else:
-			if percentChange >0:
-				print 'blue'
-				GPIO.output("P8_10", GPIO.LOW)
-				GPIO.output("P8_14", GPIO.LOW)
-				GPIO.output("P8_18",GPIO.HIGH)
-				time.sleep(10)
-			else:
-				print 'red'
-				GPIO.output("P8_10", GPIO.LOW)
-				GPIO.output("P8_14", GPIO.HIGH)
-				GPIO.output("P8_18",GPIO.LOW)
-				time.sleep(10)
